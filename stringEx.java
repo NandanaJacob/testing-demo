2d75b3f855ac1790536b3290a22a38b3d100b6e6
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2024-12-30 11:39:18 +0530

    string buffer and builder discussed

--- stringEx.java
+++ stringEx.java
@@ -1,14 +1,35 @@
 public class stringEx {
     public static void main(String[] args) {
-        String str1 = "hello";
+        String str1 = new String("hello");
         String str2 = "pooja";
 
-        System.out.println(str1.hashCode());
-        System.out.println(str2.hashCode());
+        //System.out.println(str1.hashCode());
+        //System.out.println(str2.hashCode());
 
         str1 = "welcome";
-        String str3 = "pooja";
-        System.out.println(str1.hashCode());
-        System.out.println(str3.hashCode());
+        //String str3 = "pooja";
+        //System.out.println(str1.hashCode());
+        //System.out.println(str3.hashCode());
+
+        //System.out.println(str1==str2);
+        //System.out.println(str3==str2);
+
+        //check concat
+        //System.out.println(str1.concat(str2));
+
+        //__________________________________________
+        //STRING BUFFER
+        StringBuffer sb = new StringBuffer("welcome "); //2 bytes = 16 bites
+        System.out.println(sb.capacity());
+        sb.ensureCapacity(50);
+        System.out.println(sb.append("to my session"));
+        System.out.println(sb.capacity());
+        System.out.println(sb);
+        System.out.println(sb.reverse());
+
+        //____________________________________________
+        //STRING BUILDER
     }
 }
+//str1 -> (hello,welcome)
+//str2, atr3 -> pooja
