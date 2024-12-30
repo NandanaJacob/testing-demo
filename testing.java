ecff0e67e763c9a525ef5ff1b4ca1f34059068e4
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2024-12-30 10:48:11 +0530

    greatest in array

--- testing.java
+++ testing.java
@@ -2,5 +2,17 @@ public class testing {
     public static void main(String[] args) {
         // Print Hello, World! to the console
         System.out.println("Hello, World!");
+
+        int[] arr = {10, 45,23,67,11};
+        int gr = arr[0];
+        //for(int i=0; i<arr.length; i++)
+        for(int num:arr)
+        {
+            if(gr<arr[num])
+            {
+                gr = arr[num];
+            }
+        }
+        System.out.println(gr);
     }
 }
