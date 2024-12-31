ea0ab85996a99bbc5a8b895ced8d93cbe802cac3
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2024-12-31 11:18:49 +0530

    MainEmployee enter multiple employees using constructor

--- MainEmployee.java
+++ MainEmployee.java
@@ -6,8 +6,38 @@ public class MainEmployee {
         System.out.println(e1.getName());
         System.out.println(e1.getSal());
         //System.out.println(e1);
+        //e1.toString();
 
         //read data frm user at runtime, use Scanner from java.util
         //=> import java.util.Scanner
+        Scanner sc = new Scanner(System.in);
+        //to recieve int vale
+        int id = sc.nextInt();
+        String name = sc.next();
+        int sal = sc.nextInt();
+        Employee e2 = new Employee(id, name, sal);
+        //System.out.println(e2);
+        System.out.println(e2.getId());
+        System.out.println(e2.getName());
+        System.out.println(e2.getSal());
+
+        //________________________________________________________________
+        //read multiple employees from user
+        Employee e[] = new Employee[3];
+		
+		for(int i=0;i<e.length;i++)
+		{
+            System.out.println("Enter for employee " + i);
+		    int no = sc.nextInt();
+		    String ename = sc.next();
+		    int salary = sc.nextInt();
+			
+		    e[i]=new Employee(no,ename,salary);
+		}
+		
+		for(int i=0;i<e.length;i++)
+		System.out.println(e[i]);
+
+        sc.close();
     }
 }
