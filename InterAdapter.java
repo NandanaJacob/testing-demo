488fe112bb8a09bd4d3465ff5976e1d9060972d2
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2025-01-04 12:56:18 +0530

    fixed some errors in InterAdapter example

--- InterAdapter.java
+++ InterAdapter.java
@@ -1,21 +1,26 @@
-public class InterAdapter implements Inter2
+public class InterAdapter implements inter2, inter1
 {
- 
-	@Override
-	public void method1() {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {
+		inter2 n1;
+		n1 = new InterAdapter();
+		//n1.method4(); ====> could be done in 1.8 version of java or smthing
+
 	}
  
 	@Override
+	public void method1() {
+		System.out.println("method1");
+		
+	}
+ 
 	public void method3() {
-		// TODO Auto-generated method stub
+		System.out.println("method3");
 		
 	}
  
 	@Override
 	public void method2() {
-		// TODO Auto-generated method stub
+		System.out.println("method2");
 		
 	}
  
--- inter2.java
+++ inter2.java
@@ -1,4 +1,7 @@
 public interface inter2 {
     public void method2();
+    static void method4(){
+        System.out.println("method 4");
+    }
 }
 
