d00fa07d2608fe2be84bfa789382f486ea64f101
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2025-01-04 11:37:51 +0530

    inter1, inter2, interimpl to show interface

--- interimpl.java
+++ interimpl.java
@@ -4,6 +4,13 @@ public class interimpl implements inter1, inter2{
         c1.method1();
         c1.method2();
         c1.classmethod();
+
+        //if inter2 extends innter1 and u do
+        //inter2 c1;
+        //c1 = new interimpl;
+        //c1.method1();
+        //c1.method2();
+        //c1.classmethod();  -----> will show error
     }
     void classmethod(){
         System.out.println("class method");
