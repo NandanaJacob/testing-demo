b38880ee24645e8d5fc6c9f940cace39dfa42e58
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2025-01-04 10:45:04 +0530

    overriding, overloading, super keyword, annotations

--- multilevel_inheritance.java
+++ multilevel_inheritance.java
@@ -12,6 +12,14 @@ class Parent extends Grandparent {
         int pid = 567;
         System.out.println("This is parent id= " + pid);
     }
+
+    public void disp(){
+        System.out.println("i am parent");
+    }
+    //ex for overloading
+    public void disp(int cid){
+        System.out.println("567 is parent of " + cid);
+    }
 }
 
 // Child class inheriting from Parent
@@ -19,6 +27,13 @@ class Child extends Parent {
     void familyChild() {
         int cid = 789;
         System.out.println("This is child id= " + cid);
+    }
+    @Override //use of annotations
+    public void disp(){
+        System.out.println("i am child");
+    }
+    public void disp_parent(){
+        super.disp();
     }
 }
 
@@ -32,5 +47,9 @@ public class multilevel_inheritance {
         child.familyGrandparent();      // Inherited from Grandparent
         child.familyParent();   // Inherited from Parent
         child.familyChild();   // Defined in Child
+
+        child.disp();
+        child.disp_parent();
+        child.disp(999);
     }
 }
