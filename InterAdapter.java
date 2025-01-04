8b8d952a235bfacf0f5edfa6274f4a47d89a0e0b
Nandana Lakshmi Jacob <nandanajacob@gmail.com>
2025-01-04 12:49:25 +0530

    error filler interadapter.java

---
+++ InterAdapter.java
@@ -0,0 +1,24 @@
+public class InterAdapter implements Inter2
+{
+ 
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+}
+ 
+
